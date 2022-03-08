5b1a7c0d58a665214bbba4e5adeecd61e9f765c5
Alyssa Vu <alyssa130300@gmail.com>
2022-03-08 01:00:24 -0500

    show data with recycler view

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -3,26 +3,47 @@ package com.codepath.apps.restclienttemplate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
 
 private const val TAG = "TimelineActivity"
 class TimelineActivity : AppCompatActivity() {
 
     lateinit var client: TwitterClient
+    lateinit var rvTweets: RecyclerView
+    lateinit var adapter: TweetsAdapter
+    val tweets = ArrayList<Tweet>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
 
         client = TwitterApplication.getRestClient(this)
+
+        rvTweets = findViewById(R.id.rvTweets)
+        adapter = TweetsAdapter(tweets)
+
+        rvTweets.layoutManager = LinearLayoutManager(this)
+        rvTweets.adapter = adapter
+
         getHomeTimeline()
     }
 
     fun getHomeTimeline() {
         client.populateHomeTimeline(object: JsonHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                Log.i(TAG, "onSuccess $json")
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess")
+                try {
+                    val jsonArray = json.jsonArray
+                    tweets.addAll(Tweet.fromJsonArray(jsonArray))
+                    adapter.notifyDataSetChanged()
+                } catch (e: JSONException) {
+                    Log.e(TAG, "JSON Exception $e")
+                }
             }
 
             override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
