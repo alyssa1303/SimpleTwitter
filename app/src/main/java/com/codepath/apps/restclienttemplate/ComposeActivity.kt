877ca2c17027c111371ab68cb3e654030665c636
Alyssa Vu <alyssa130300@gmail.com>
2022-03-14 16:56:42 -0400

    Finish all user stories

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,11 +1,15 @@
 package com.codepath.apps.restclienttemplate
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -16,6 +20,7 @@ private const val TAG = "ComposeActivity"
 class ComposeActivity : AppCompatActivity() {
     private lateinit var etCompose: EditText
     private lateinit var btnTweet: Button
+    private lateinit var tvCount: TextView
     lateinit var client: TwitterClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +29,33 @@ class ComposeActivity : AppCompatActivity() {
 
         etCompose = findViewById(R.id.composeTweet)
         btnTweet = findViewById(R.id.btnTweet)
+        tvCount = findViewById(R.id.tvCharCount)
 
         client = TwitterApplication.getRestClient(this)
+
+        val limit = 280
+        etCompose.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // Fires right as the text is being changed (even supplies the range of text)
+
+                if (s.length > limit) {
+                    tvCount.setTextColor(Color.RED)
+                    btnTweet.isEnabled = false
+                } else {
+                    tvCount.setTextColor(Color.GRAY)
+                    btnTweet.isEnabled = true
+                }
+                tvCount.setText((limit - s.length).toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Fires right before text is changing
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Fires right after the text has changed
+            }
+        })
 
         // Handling the user's click on the tweet button
         btnTweet.setOnClickListener{
@@ -37,8 +67,8 @@ class ComposeActivity : AppCompatActivity() {
                 Toast.makeText(this, "Empty tweet not allowed!", Toast.LENGTH_SHORT).show()
             }
             // 2. Make sure tweet is less than character count
-            else if (tweetContent.length > 140) {
-                Toast.makeText(this, "Tweet exceeds limit of 140 characters", Toast.LENGTH_SHORT).show()
+            else if (tweetContent.length > limit) {
+                Toast.makeText(this, "Tweet exceeds limit of 280 characters", Toast.LENGTH_SHORT).show()
             } else {
                 client.postTweet(object: JsonHttpResponseHandler() {
                     override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
